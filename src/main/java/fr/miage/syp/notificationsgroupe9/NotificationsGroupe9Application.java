eed564aae8be326fc8fdb3cefd6ede18222e0ba4
Maxime Galaxy Book <max.sevot@gmail.com>
2025-03-31 18:53:15 +0200

    Init commit

---
+++ src/main/java/fr/miage/syp/notificationsgroupe9/NotificationsGroupe9Application.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.notificationsgroupe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsGroupe9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationsGroupe9Application.class, args);
+    }
+
+}
