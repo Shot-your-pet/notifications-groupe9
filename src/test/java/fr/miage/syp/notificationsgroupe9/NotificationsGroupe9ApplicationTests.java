527f74c3b4f69f1b3725b3999a4672ae42edd360
Maxime Galaxy Book <max.sevot@gmail.com>
2025-03-31 18:53:15 +0200

    Init commit

---
+++ src/test/java/fr/miage/syp/notificationsgroupe9/NotificationsGroupe9ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.notificationsgroupe9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsGroupe9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
